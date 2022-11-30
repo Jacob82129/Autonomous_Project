0e598b74ddb69b31e33606d489ab2ffe687089dd
Jacob Navarro <navarrojacob532@gmail.com>
2022-11-29 21:54:15 -0800

    Final Code Version

--- lab_8_psuedo_code/lab_8_psuedo_code.ino
+++ lab_8_psuedo_code/lab_8_psuedo_code.ino
@@ -11,7 +11,7 @@
 #define PWM_B_Pin 11 // PWM B
 #define DIR_B_Pin 4 // DIR B
 
-#define s0 4        //Module pins wiring
+#define s0 4        //Module pins wiring for Color Sensor
 #define s1 5
 #define s2 6
 #define s3 10
@@ -21,11 +21,11 @@
 const int trackingPin1 = 7;
 const int trackingPin2 = 8; 
 
+// Flag Variables for Tracking Algorithm
 bool leftFlag = false;
 bool rightFlag = false;
 bool straightFlag = false;
 bool stopFlag = false;
-//bool colorFlag = false;
 
 int speedA;
 int speedB;
@@ -40,7 +40,10 @@ int frequencyR = 0;
 int frequencyG = 0;
 int frequencyB = 0;
 
-//int interrupt_pin = s2;
+const int interrupt_pin = 3;
+//volatile int objectFlag;
+
+int buzzerPin = 1;
 
 void setup() {
   
@@ -50,8 +53,8 @@ void setup() {
   pinMode(trackingPin1, INPUT); // set trackingPin1 as INPUT
   pinMode(trackingPin2, INPUT);  //set trackingPin2 as INPUT
 
-  //pinMode(interrupt_pin, INPUT_PULLUP);
-  //attachInterrupt(digitalPinToInterrupt(interrupt_pin), stopColor, CHANGE);
+  pinMode(interrupt_pin, INPUT_PULLUP);
+  attachInterrupt(digitalPinToInterrupt(interrupt_pin), halt, CHANGE);
   
   
   pinMode(s0,OUTPUT);    //pin modes
@@ -67,7 +70,9 @@ void setup() {
   Serial.begin(9600);
 
   digitalWrite(s0,HIGH); //Putting S0/S1 on HIGH/HIGH levels means the output frequency scalling is at 100% (recommended)
-  digitalWrite(s1,HIGH); //LOW/LOW is off HIGH/LOW is 20% and LOW/HIGH is  2%
+  digitalWrite(s1,HIGH); 
+
+  pinMode(buzzerPin, OUTPUT);// setting up buzzer as an output
  
 }
 
@@ -81,14 +86,13 @@ void loop()
   
   if(val1 == HIGH && val2 == HIGH && (straightFlag == false))
   {
-    goStraight(forwardA, 230);
+    goStraight(forwardA, 250);
     straightFlag == true;
   }
   else if(val1 == LOW && val2 == HIGH && (leftFlag == false))
   {
     turnLeft();
-    leftFlag == true;
-    
+    leftFlag == true;  
   }
   else if(val1 == HIGH && val2 == LOW && (rightFlag == false))
   {
@@ -99,12 +103,14 @@ void loop()
   {
     stopMotion();
     stopFlag == true;
-  }    
+  }
 
   stopColor();
+
 }
 
-void motionA(int directionA, int speedA){
+void motionA(int directionA, int speedA)
+{
 
    //forward @ full speed
   digitalWrite(DIR_A_Pin, directionA); //Establishes forward direction of Channel A
@@ -162,7 +168,7 @@ void stopColor()
   frequencyR = pulseIn(out, LOW);
   frequencyR = map(frequencyR, 25, 70, 255, 0);
   //Printing the value on the serial monitor
-  /*
+  /* //Check Red Sensor Variables
   Serial.print("R = ");
   Serial.print(frequencyR);
   Serial.print(" ");
@@ -173,7 +179,7 @@ void stopColor()
   digitalWrite(s3, HIGH);
   frequencyG = pulseIn(out, LOW);
   frequencyG = map(frequencyG, 25, 70, 255, 0);
-/*  
+/*  //Check Green Sensor Variables
   Serial.print("G = ");
   Serial.print(frequencyG);
   Serial.print(" ");
@@ -184,23 +190,34 @@ void stopColor()
   digitalWrite(s3, HIGH);
   frequencyB = pulseIn(out, LOW);
   frequencyB = map(frequencyB, 25, 70, 255, 0);
-  /*
+  /* //Check Blue Sensor Variables
   Serial.print("B = ");
   Serial.print(frequencyB);
   Serial.print(" ");
   Serial.println("");
   delay(100);
 */
-  if(frequencyR >= 345 && frequencyG <= 210 && frequencyB <= 255) 
+  if(frequencyR >= 345 && frequencyG <= 210 && frequencyB <= 255) // variables for each color based on what the sensor acquired from sensing our red stop marker
   {      
      Serial.println("Red");
       stopMotion();
       Serial.print("Stopping");
       Serial.print('\n');
       delay(5000);
-      goStraight(forwardA, 255);
-      delay(3000);
-      //colorFlag == true;
+      goStraight(forwardA, 230);
+      delay(1000);
   }
 
+}
+
+void halt()
+{
+
+  while(digitalRead(interrupt_pin) == LOW)
+  {
+    stopMotion();
+    digitalWrite(buzzerPin, LOW); // buzzes
+    Serial.println("Stop");
+  }
+  
 }
